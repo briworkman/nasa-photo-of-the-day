6aaf39756b93b8001a56c1434ff75fde81e007fa
Brianna Workman <brianna.workman@hotmail.com>
2019-10-09 17:27:58 -0500

    Adds Some Stretch

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -7,10 +7,14 @@ const Picture = () => {
   const [title, setTitle] = useState("");
   const [explanation, setExplanation] = useState("");
 
+  const [newDate, setNewDate] = useState("");
+
   useEffect(() => {
     function getPhoto() {
       axios
-        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+        .get(
+          "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${newDate}"
+        )
         .then(response => {
           console.log(response.data);
           setPicture(response.data.url);
@@ -27,7 +31,20 @@ const Picture = () => {
   return (
     <div>
       <h2>{title}</h2>
-      <p>{date}</p>
+      <div className="dropdown">
+        <p>
+          {date} <i className="fas fa-angle-down"></i>
+        </p>
+        <div className="dropdown-content">
+          <button onClick={() => setNewDate("2018-10-09")}>One Year Ago</button>
+          <button onClick={() => setNewDate("2014-10-09")}>
+            Five Years Ago
+          </button>
+          <button onClick={() => setNewDate("2009-10-09")}>
+            Ten Years Ago
+          </button>
+        </div>
+      </div>
       <p>{explanation}</p>
       <img src={picture} alt="Nasa astronomy pic of the day" />
     </div>
